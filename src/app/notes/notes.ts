49ca75de9a187d763ae1523d42c3e2b9b760e9a7
Jason Victorio <jason@jasonvictorio.com>
2026-01-06 15:36:06 +1300

    feat: implement panning functionality for notes with mouse events and update styles

--- src/app/notes/notes.ts
+++ src/app/notes/notes.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core'
+import { Component, HostListener, signal } from '@angular/core'
 import { nanoid } from 'nanoid'
 import { NoteComponent } from './note/note'
 
@@ -31,10 +31,49 @@ const dummyNotes: Note[] = [
   imports: [NoteComponent],
   templateUrl: './notes.html',
   styleUrl: './notes.scss',
-  host: { class: 'notes' },
+  host: {
+    class: 'notes',
+    '[class.notes--panning]': 'isPanning()',
+    '[style.background-position]': 'styleBackgroundPosition()',
+  },
 })
 export class NotesComponent {
   notes = signal<Note[]>(dummyNotes)
+  isPanning = signal<boolean>(false)
+  styleTransform = signal('translate(0px, 0px)')
+  styleBackgroundPosition = signal('0px 0px')
+  offset = signal({ x: 0, y: 0 })
+
+  @HostListener('mousedown', ['$event'])
+  onMouseDown(event: MouseEvent) {
+    if (event.button === 1) {
+      event.preventDefault()
+      this.isPanning.set(true)
+    }
+  }
+
+  // TODO: remove "always" listener
+  @HostListener('window:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    if (!this.isPanning()) return
+
+    this.offset.update((p) => {
+      const newX = p.x + event.movementX
+      const newY = p.y + event.movementY
+      this.styleBackgroundPosition.set(`${newX}px ${newY}px`)
+      this.styleTransform.set(`translate(${newX}px, ${newY}px)`)
+      return { x: newX, y: newY }
+    })
+  }
+
+  // TODO: remove "always" listener
+  @HostListener('window:mouseup', ['$event'])
+  onMouseUp(event: MouseEvent) {
+    console.log('mouseup', event.button)
+    if (event.button === 1) {
+      this.isPanning.set(false)
+    }
+  }
 
   onUpdate(note: Note) {
     this.notes.update((notes) =>
